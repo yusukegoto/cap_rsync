b098b3dee36e8caa7b32739f1df688767e329bec
Yusuke Goto <u.suke.goto@gmail.com>
2016-12-17 09:12:42 +0900

    vm ruby 2.3.3

---
+++ cookbooks/cap_rsync/recipes/default.rb
@@ -0,0 +1,32 @@
+%w(epel-release gcc-c++ git openssl-devel readline-devel zlib-devel).each do |pkg|
+  package pkg
+end
+
+tmp_dir = Chef::Config['file_cache_path']
+ruby_build_path = File.join tmp_dir, 'ruby-build'
+
+git ruby_build_path do
+  repository 'https://github.com/rbenv/ruby-build.git'
+end
+
+execute 'install ruby-build' do
+  command "#{ruby_build_path}/install.sh"
+end
+
+ruby_version = '2.3.3'
+ruby_path = "/opt/ruby-#{ruby_version}"
+
+execute 'install ruby' do
+  not_if  "test -d #{ruby_path}"
+  command "/usr/local/bin/ruby-build #{ruby_version} #{ruby_path}"
+end
+
+link '/opt/ruby' do
+  to ruby_path
+end
+
+%w(erb gem irb rake rdoc ri ruby).each do |cmd|
+  link "/usr/bin/#{cmd}" do
+    to "/opt/ruby/bin/#{cmd}"
+  end
+end
